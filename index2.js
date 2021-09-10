c912bd0582a0053854e3aedb1ebe5b5fc2f50440
skeanster <ryanskeans@gmail.com>
2021-09-10 16:48:15 -0700

    more mobile responsive

--- index2.js
+++ index2.js
@@ -1,8 +1,9 @@
 const buttons = document.querySelector('#select')
+const buttonsTwo = document.querySelector('#selectTwo')
 const reload =document.querySelector('#reload')
 const mario =document.querySelector('#marioHide')
 const bowser =document.querySelector('#bowserHide')
-
+const message =document.getElementById('message')
 
 
 function playRound(playerNum,computerNum) {
@@ -11,18 +12,23 @@ function playRound(playerNum,computerNum) {
 
     if (playerNum==computerNum) {
         console.log(1)
+        message.innerHTML = "tie, try again!"
+
     }
 
     else if (playerNum==0 && computerNum==1) {
         var computerScore = document.getElementById("computerScore");
         computerScore.innerHTML = parseInt(computerScore.innerHTML)+parseInt(1)
         console.log(2)
+        message.innerHTML = "bowser wins this round..."
+
     }
 
     else if (playerNum==0 && computerNum==2) {
         var playerScore = document.getElementById("playerScore");
         playerScore.innerHTML = parseInt(playerScore.innerHTML)+parseInt(1)
         console.log(3)
+        message.innerHTML = "great job! keep going!"
 
     }
 
@@ -30,6 +36,7 @@ function playRound(playerNum,computerNum) {
         var playerScore = document.getElementById("playerScore");
         playerScore.innerHTML = parseInt(playerScore.innerHTML)+parseInt(1)
         console.log(4)
+        message.innerHTML = "great job! keep going!"
 
     }
 
@@ -37,6 +44,7 @@ function playRound(playerNum,computerNum) {
         var computerScore = document.getElementById("computerScore");
         computerScore.innerHTML = parseInt(computerScore.innerHTML)+parseInt(1)
         console.log(5)
+        message.innerHTML = "bowser wins this round..."
 
     }
 
@@ -44,6 +52,7 @@ function playRound(playerNum,computerNum) {
         var playerScore = document.getElementById("playerScore");
         playerScore.innerHTML = parseInt(playerScore.innerHTML)+parseInt(1)
         console.log(6)
+        message.innerHTML = "great job! keep going!"
 
     }
 
@@ -51,6 +60,7 @@ function playRound(playerNum,computerNum) {
         var computerScore = document.getElementById("computerScore");
         computerScore.innerHTML = parseInt(computerScore.innerHTML)+parseInt(1)
         console.log(7)
+        message.innerHTML = "bowser wins this round..."
 
     }
 }
@@ -60,14 +70,20 @@ function over() {
 
     if (playerScore.innerHTML==5){
         buttons.classList.add('hide')
+        buttonsTwo.classList.add('hide')
         reload.classList.remove('hide')
         mario.classList.remove('hide')
+        message.innerHTML = "wahoo! you did it!"
+
     }
 
     else if (computerScore.innerHTML==5){
         buttons.classList.add('hide')
+        buttonsTwo.classList.add('hide')
         reload.classList.remove('hide')
         bowser.classList.remove('hide')
+        message.innerHTML = "are you really gonna let him laugh at you?"
+
     }
 
 }
